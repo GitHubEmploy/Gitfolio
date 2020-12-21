150836f2b14918635fcd0f0fe69d548af2eb38a4
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-12-21 11:08:22 -0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+npm i gitfolio -g
+gitfolio build GitHubEmploy --sort star --order desc --theme dark --background https://www.blogenium.com/wp-content/uploads/2019/08/blogenium-cool-wallpapers-pictures-1.jpg --twitter MohitVarikuti --linkedin MohitVarikuti --medium MohitVarikuti
+gitfolio run -p 5000
